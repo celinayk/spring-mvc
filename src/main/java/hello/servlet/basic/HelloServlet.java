ae164b21692bf58ae909b4ee3425f47cf219a002
YeonKyung Ryu <celina16161616@gmail.com>
2025-09-19 21:22:20 +0900

    서블릿, jsp로 구현하기

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+  // 이 서블릿이 호출되면 이 메서드가 실행됨
+  @Override
+  protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    System.out.println("HelloServlet.service");
+    System.out.println("req = " + req);
+    System.out.println("resp = " + resp);
+
+    String username = req.getParameter("username");
+    System.out.println("username = " + username);
+
+    resp.setContentType("text/plain");
+    resp.setCharacterEncoding("utf-8");
+    resp.getWriter().write("Hello " + username);
+
+
+  }
+}
