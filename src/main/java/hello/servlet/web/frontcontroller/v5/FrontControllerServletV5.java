d597db611c83b85a3699fdef33a50ad9c22c483d
YeonKyung Ryu <celina16161616@gmail.com>
2025-09-21 16:37:47 +0900

    FrontController V5-2 v4버전도 사용할 수 있도록함

--- src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -6,6 +6,9 @@ import hello.servlet.web.frontcontroller.v3.ControllerV3;
 import hello.servlet.web.frontcontroller.v3.MemberFormControllerV3;
 import hello.servlet.web.frontcontroller.v3.MemberListControllerV3;
 import hello.servlet.web.frontcontroller.v3.MemberSaveControllerV3;
+import hello.servlet.web.frontcontroller.v4.MemberFormControllerV4;
+import hello.servlet.web.frontcontroller.v4.MemberListControllerV4;
+import hello.servlet.web.frontcontroller.v4.MemberSaveControllerV4;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -28,12 +31,18 @@ public class FrontControllerServletV5 extends HttpServlet {
 
   private void initHandlerAdapters() {
     handlerAdapters.add(new ControllerV3HandlerAdapter());
+    handlerAdapters.add(new ControllerV4HandlerAdapter()); //V4 추가
   }
 
   private void initHandlerMappingMap() {
     handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
     handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
     handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+
+    //V4 추가
+    handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+    handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+    handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
   }
 
   @Override
